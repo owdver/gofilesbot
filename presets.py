4cb518f0fbee2cabcd250ac632cf7aeb79a62001
I'M OWDVER <56756946+owdver@users.noreply.github.com>
2021-11-11 16:50:35 +0530

    added help text and about text!

--- presets.py
+++ presets.py
@@ -1,22 +1,39 @@
 # ----------------------------------- https://github.com/m4mallu/gofilesbot ------------------------------------------ #
 
 class Presets(object):
-    CAPTION_TEXT_DOC = "\n\n<b>File Name:</b> {}\n\n<b>Format:</b> {}\n<b>Size:</b> {}"
-    CAPTION_TEXT_VID = "\n\n<b>File Name:</b> {}\n\n<b>Size:</b> {}"
+    CAPTION_TEXT_DOC = "<code>{}</code>\n\n<b>♻️ 𝙅𝙊𝙄𝙉 :- https://t.me/joinchat/Ur8bdKHGNRdBoeHW</b>"
+    
+    CAPTION_TEXT_VID = "<code>{}</code>\n\n<b>♻️ 𝙅𝙊𝙄𝙉 :- https://t.me/joinchat/Ur8bdKHGNRdBoeHW</b>"
+    
     ASK_PM_TEXT = "<b>Click the below button</b>"
+    
     WELCOME_TEXT = "Hello.. <b>{}</b>\n<code>I can help you getting movies from</code> @MovieKeralam. " \
                    "<code>Just Keep this message live Here</code>😉\n\n" \
                    "<b>My code can be seen: </b><a href='https://github.com/m4mallu/gofilesbot'> HERE</a>"
+    
     CLEAN_CHAT_MSG = "⚠️ <b>Deleting all messages..</b>"
+    
     MSG_FOR_PIN = "<b>For getting medias from here..</b>\n\n🔛 <code>Please start</code> @{} <code>in PM\n\n" \
                   "Send the exact Movie name.\n\n🔊 I'll reply the file in PM if available in our channel !</code>"
 
     BOT_PM_TEXT = "<b>Sorry.. 😢</b>\n\n<code>Bot won't work in PM, Ask in ma Group. I'll reply the file in PM if " \
                   "available in our DB !</code>"
+    
     PM_ERROR = "<b>Unable to send medias</b> ⛔️\n<code>Click the below button\nAsk here for movies later!</code>"
+    
     MEDIA_SEND_TEXT = "<code>Media dispatched as PM 🥳</code>"
+    
     NO_MEDIA = "Requested movie: <b>{}</b>\n\n<b>Not available " \
                "Right Now</b>\n<code>Possible Causes : 🤔\n\n⭕️ Not " \
                "released yet</code>\n⭕️ <a href='https://www.google.com/search?q={}'> Spelled incorrectly</a>\n" \
                "<code>⭕️ Unwanted texts in Msgs\n⭕ Asking theatre prints\n⭕ Not in ma Database</code>"
+    
     BLOCK_LIST = ['http://', 'https://', '@', '#', 'bit.ly', 't.me', '/']
+    
+    
+    
+    #Added By @OwDvEr!
+    
+        HELP_TXT = """Hey {}\nHere Is The Help For Using This Bot"""
+        
+        ABOUT_TXT ="""✯ Mʏ Nᴀᴍᴇ: Sᴄᴀʀʟᴇᴛᴛ Jᴏʜᴀɴssᴏɴ\n✯ Cʀᴇᴀᴛᴏʀ: Oᴡᴅᴠᴇʀ Bᴏᴛ\n✯ Lɪʙʀᴀʀʏ: Pʏʀᴏɢʀᴀᴍ\n✯ Lᴀɴɢᴜᴀɢᴇ: Pʏᴛʜᴏɴ 3\n✯ Bᴏᴛ Sᴇʀᴠᴇʀ: Hᴇʀᴏᴋᴜ\n✯ Bᴜɪʟᴅ Sᴛᴀᴛᴜs: ᴠ1.0.6 [ Bᴇᴛᴀ ]"""
