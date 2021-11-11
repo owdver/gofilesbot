108c6abf5756f1e4ae71dda68e4336dd503419a7
I'M OWDVER <56756946+owdver@users.noreply.github.com>
2021-11-11 12:45:56 +0530

    Update pm_query.py

--- plugins/pm_query.py
+++ plugins/pm_query.py
@@ -45,7 +45,7 @@ async def start(client, message):
         base64_bytes = b64decode(query_bytes)
         secret_query = base64_bytes.decode("ascii")
     except Exception:
-            pass
+        return
         if secret_query:
             for channel in Config.CHANNELS:
                 # Looking for Document type in messages
