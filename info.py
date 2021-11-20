d53a1e75b71ce978bdc2dcb9b096d3f384f3ec42
I'M OWDVER <56756946+owdver@users.noreply.github.com>
2021-11-20 15:32:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,6 +3,7 @@ from os import environ
 id_pattern = re.compile(r'^.\d+$')
 
 
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('AUTH_USERS', '').split()]
 auth_channel = environ.get('AUTH_CHANNEL')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
 invite_link = "https://t.me/joinchat/Ur8bdKHGNRdBoeHW"
