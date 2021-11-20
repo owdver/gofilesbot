12cd7642fc1e15e8c1ff84d6756c0071b5af2df9
I'M OWDVER <56756946+owdver@users.noreply.github.com>
2021-11-20 15:15:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,3 +5,4 @@ id_pattern = re.compile(r'^.\d+$')
 
 auth_channel = environ.get('AUTH_CHANNEL')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
+invite_link = "https://t.me/joinchat/Ur8bdKHGNRdBoeHW"
