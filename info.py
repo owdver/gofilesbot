4da24c95a793b046436d8e09fc1262e3c0312139
I'M OWDVER <56756946+owdver@users.noreply.github.com>
2021-11-20 15:04:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,11 +2,6 @@ import re
 from os import environ
 id_pattern = re.compile(r'^.\d+$')
 
-# Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', "").split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
-AUTH_USERS = (auth_users + ADMINS) if auth_users else []
+
 auth_channel = environ.get('AUTH_CHANNEL')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
-AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
