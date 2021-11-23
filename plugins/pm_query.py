2f197e0afcaadd0c99bbc10037437f9de1279d8b
I'M OWDVER <56756946+owdver@users.noreply.github.com>
2021-11-23 19:04:32 +0530

    Update pm_query.py

--- plugins/pm_query.py
+++ plugins/pm_query.py
@@ -38,7 +38,7 @@ async def bot_pm(client: Bot, message: Message):
         )
     if AUTH_CHANNEL and not await is_subscribed(client, message):
         try:
-            invite_link = await client.export_chat_invite_link({CHANNEL_USERNAME})
+            invite_link = await client.export_chat_invite_link(int(AUTH_CHANNEL))
         except ChatAdminRequired:
             logger.error("Mᴀᴋᴇ Sᴜʀᴇ Bᴏᴛ Is Aᴅᴍɪɴ Iɴ FᴏʀᴄᴇSᴜʙ Cʜᴀɴɴᴇʟ")
             return
